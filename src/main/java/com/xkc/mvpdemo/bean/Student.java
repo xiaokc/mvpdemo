4e70260f57b2b3a91722341b3c4c5a5829e156a5
xkc <1337154470@qq.com>
2016-09-08 22:26:34 +0800

    完成bean Student

--- src/main/java/com/xkc/mvpdemo/bean/Student.java
+++ src/main/java/com/xkc/mvpdemo/bean/Student.java
@@ -4,4 +4,31 @@ package com.xkc.mvpdemo.bean;
  * Created by xkc on 9/8/16.
  */
 public class Student {
+    private String id;
+    private String name;
+    private String gender;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
 }
